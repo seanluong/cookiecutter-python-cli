a7d0aeb38ec46972858439a812a1399e2fd7f3d1
Xuan Luong <luonghoaixuan@gmail.com>
2017-10-25 21:10:39 -0400

    Add post generation hook

--- tests/test_cookiecutter_python_cli.py
+++ tests/test_cookiecutter_python_cli.py
@@ -1,18 +1,48 @@
-from cookiecutter.main import cookiecutter
+# from contextlib import ContextDecorator, ContextBaseClass
 import os
 from os.path import dirname, abspath
+import shutil
+import tempfile
 import pytest
+from cookiecutter.main import cookiecutter
+import sys
+import subprocess
 
 
-def test_project_tests_passed(tmpdir):
+
+@pytest.fixture
+def tmp_dir_path():
+    tmp_dir_path = tempfile.mkdtemp(suffix='cookiecutter', dir='/tmp')
+    yield tmp_dir_path
+    if os.path.exists(tmp_dir_path):
+        shutil.rmtree(tmp_dir_path)
+
+
+def test_project_tests_passed(tmp_dir_path):
     template = dirname(dirname(abspath(__file__)))
     extra_context = {
         'project_name': 'mycli'
     }
-    output_dir = str(tmpdir.realpath())
+
     result = cookiecutter(template,
                           no_input=True,
                           extra_context=extra_context,
-                          output_dir=output_dir)
+                          output_dir=tmp_dir_path)
 
-    assert pytest.main([os.path.join(output_dir, 'mycli')]) == 0
+    assert subprocess.check_call(['pytest', os.path.join(tmp_dir_path, 'mycli')]) == 0
+
+
+def test_project_no_license(tmp_dir_path):
+    template = dirname(dirname(abspath(__file__)))
+    extra_context = {
+        'project_name': 'mycli',
+        'open_source_license': 'Not open source'
+    }
+
+    result = cookiecutter(template,
+                          no_input=True,
+                          extra_context=extra_context,
+                          output_dir=tmp_dir_path)
+
+    assert subprocess.check_call(['pytest', os.path.join(tmp_dir_path, 'mycli')]) == 0
+    assert not os.path.exists(os.path.join(tmp_dir_path, 'mycli', 'LICENSE'))
